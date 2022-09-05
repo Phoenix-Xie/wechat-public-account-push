a2b8c17b43a5ecf9aff49fb5daa19661cdff68e6
小谢 <32811223+Phoenix-Xie@users.noreply.github.com>
2022-09-05 16:39:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ export const config = {
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "gWXCb7CTjW65Lw9WiaRekqj8Bw57Ec6MUJUcseXtqFg",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
+   *    openUrl: "https://s.weibo.com/top/summary?cate=realtimehot"
    *   }, 
    * ]
    */
@@ -31,7 +31,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oviKO55MArjqeg0SEv6sJu8J9McA",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "CHF8VhYm6E1-DVPobfbiLld1jk22bwDwOv4Wn-SvdmQ",
+      useTemplateId: "gWXCb7CTjW65Lw9WiaRekqj8Bw57Ec6MUJUcseXtqFg",
       // 所在省份
       province: "山东",
       // 所在城市
